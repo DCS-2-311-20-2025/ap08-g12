af40f1a3bf5874514363fdb7e6102fa00f48e013
G485122024 <248512@st.takushoku-u.ac.jp>
2025-11-19 14:59:58 +0900

    L3

--- ap08L3.js
+++ ap08L3.js
@@ -112,7 +112,7 @@ export function render(scene, car) {
     course.getPointAt(time % 1, carPosition);
     car.position.copy(carPosition);
     course.getPointAt((time + 0.01) % 1, carTarget);
-    car.lookAt(carTarget);
+    car.lookAt(carTarget);h
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
